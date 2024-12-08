6a8d7f8a4924c917584e43a39c32fe5c26f0a717
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2024-12-08 14:42:38 -0600

    Prepare issue

--- c/include/tbe.h
+++ c/include/tbe.h
@@ -1,41 +1,39 @@
 #ifndef TBE_H
 #define TBE_H
 
-#include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 #define MAX_NAME_LEN 256
 #define MAX_VALUE_LEN 256
 
+// Attribute structure
 typedef struct Attribute {
     char name[MAX_NAME_LEN];
     char value[MAX_VALUE_LEN];
     struct Attribute* next;
 } Attribute;
 
+// TBL Section structure
 typedef struct TBLSection {
     char name[MAX_NAME_LEN];
     Attribute* attributes;
     struct TBLSection* next;
 } TBLSection;
 
+// TBE Header structure
 typedef struct TBEHeader {
     Attribute* bgn_attributes;
     Attribute* eot_attributes;
     TBLSection* sections;
 } TBEHeader;
 
-// Parser function
+// Function prototypes
 TBEHeader* parse_TBE_header(const char* filename);
-
-// Exporter function
-int export_TBE(const TBEHeader* header, const char* filename);
-
-// Free memory function
 void free_tbe_header(TBEHeader* header);
-
-// Print function for debugging
 void print_tbe_header(const TBEHeader* header);
+int export_TBE(const TBEHeader* header, const char* filename);
+void add_attribute(Attribute** head, const char* name, const char* value);
 
-#endif
+#endif // TBE_H
